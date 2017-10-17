73562aae09d7c5b4ab6cd5296c698e36fcab81fa
Aleksei Proskurnov <proskurn@gmail.com>
2017-10-17 14:21:34 +0300

    compile

--- dist/maskedInput.js
+++ dist/maskedInput.js
@@ -70,6 +70,7 @@ export default {
     },
     value: function value(newValue) {
       if (this.maskCore) this.maskCore.setValue(newValue); // For multiple inputs support
+      this.updateToCoreState();
     }
   },
 
@@ -128,6 +129,7 @@ export default {
                 }
               }
             }
+            /* eslint-enable */
           });
         }
         [].concat(_toConsumableArray(this.$refs.input.value)).reduce(function (memo, item) {
@@ -314,6 +316,7 @@ export default {
         });
         this.$emit('input', '', '');
       }
+      this.$emit('blur');
     },
     setNativeSelection: function setNativeSelection() {
       this.maskCore.selection = {
